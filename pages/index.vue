1001bcd6bcc7b072798f3b1a01358bc30ef238f6
unknown <helio@solucx.com.br>
2021-01-25 21:00:02 -0300

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+      <h1>auhehuae</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container{
+  max-width: 1280px;
+  padding: 0 80px;
+}
+</style>
